bbba9412e8e62781c12a14efb3728199cf6d01fc
ndtrang121 <ndtrang129@gmail.com>
2022-08-02 11:39:15 +0700

    Project restructuring

--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -4,7 +4,7 @@ import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import HeadlessTippy from '@tippyjs/react/headless'
 import classNames from 'classnames/bind'
 
-import * as searchServices from '~/apiServices/searchServices'
+import * as searchServices from '~/services/searchService'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
 import AccountItem from '~/components/AccountItem'
 import styles from './Search.module.scss'
@@ -76,9 +76,11 @@ function Search() {
 					<div className={cx('search-result')} tabIndex='-1' {...attrs}>
 						<PopperWrapper>
 							<h4 className={cx('search-title')}>Accounts</h4>
-							{searchResult.map((result) => (
-								<AccountItem key={result.id} data={result} onClick={handleHideResult} />
-							))}
+							<div className={cx('account-items')}>
+								{searchResult.map((result) => (
+									<AccountItem key={result.id} data={result} onClick={handleHideResult} />
+								))}
+							</div>
 						</PopperWrapper>
 					</div>
 				)}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { HeaderOnly } from '~/components/Layout'
+import { HeaderOnly } from '~/layouts'
 import routesConfig from '~/config/routes'
 
 import Home from '~/pages/Home'
